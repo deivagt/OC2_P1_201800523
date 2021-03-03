d92dfd0ac90e80d6397fa6d922d097964ee36d35
David Alberto Avila Belisle <david1alberto@gmail.com>
2021-03-02 19:46:03 -0600

    Ahora los terminales y no terminales son publicos para ser usados en a ejecucion

--- AST/Gramatica.cs
+++ AST/Gramatica.cs
@@ -17,66 +17,61 @@ namespace OC2_P1_201800523.AST
              
              
              */
-
             
             #region Declaracion de Terminales
-            IdentifierTerminal id = new IdentifierTerminal("id");
-            NumberLiteral numero = new NumberLiteral("numero");
-            //var id = ToTerm("id");
-            var program = ToTerm("program");
-            var punto_coma = ToTerm(";");
-            var punto = ToTerm(".");
-            var uses = ToTerm("uses");
-            var coma = ToTerm(",");
-            var var = ToTerm("var");
-            var rconst = ToTerm("const");
+            IdentifierTerminal id = new IdentifierTerminal(terminales.id);
+            NumberLiteral numero = new NumberLiteral(terminales.numero);            
+            var program = ToTerm(terminales.program);           
+            var var = ToTerm(terminales.var);
+            var rconst = ToTerm(terminales.rconst);
+            var function = ToTerm(terminales.function);
+            var begin = ToTerm(terminales.begin);
+            var end = ToTerm(terminales.end);
 
-            var function = ToTerm("function");
-            var abrir_parentesis = ToTerm("(");
-            var cerrar_parentesis = ToTerm(")");
-            var dos_puntos_igual = ToTerm(":=");
-            var dos_puntos = ToTerm(":");
-            var igual = ToTerm("=");
+            /*TIPOS*/
+            var rstring = ToTerm(terminales.rstring);
+            var rinteger = ToTerm(terminales.rinteger);
+            var rreal = ToTerm(terminales.rreal);
+            var rchar = ToTerm(terminales.rchar);
+            var rboolean = ToTerm(terminales.rboolean);
+            var rvoid = ToTerm(terminales.rvoid);
 
-
-            /***************/
-
-            var rstring = ToTerm("string");
-            var rinteger = ToTerm("integer");
-            var rreal = ToTerm("real");
-            var rchar = ToTerm("char");
-            var rboolean = ToTerm("boolean");
-            var rvoid = ToTerm("void");
-            /***************/
-
-            var begin = ToTerm("begin");
-            var end = ToTerm("end");
+            /*SIMBOLOS*/
+            var punto_coma = ToTerm(terminales.punto_coma);
+            var punto = ToTerm(terminales.punto);
+            var uses = ToTerm(terminales.uses);
+            var coma = ToTerm(terminales.coma);
+            var abrir_parentesis = ToTerm(terminales.abrir_parentesis);
+            var cerrar_parentesis = ToTerm(terminales.cerrar_parentesis);
+            var dos_puntos_igual = ToTerm(terminales.dos_puntos_igual);
+            var dos_puntos = ToTerm(terminales.dos_puntos);
+            var igual = ToTerm(terminales.igual);
 
             #endregion
 
             #region No terminales
-            NonTerminal INI = new NonTerminal("INI");
-            NonTerminal COSAS = new NonTerminal("COSAS");
-            NonTerminal USES = new NonTerminal("USES");
-            NonTerminal CUERPO_PROGRAMA = new NonTerminal("CUERPO_PROGRAMA");
-            NonTerminal INSTRUCCIONES = new NonTerminal("INSTRUCCIONES");
-            NonTerminal OTRO_USES = new NonTerminal("OTRO_USES");
-            NonTerminal INSTRUCCION = new NonTerminal("INSTRUCCION");
-            NonTerminal VARIABLE = new NonTerminal("VARIABLE");
-            NonTerminal ASIGNAR_VALOR = new NonTerminal("ASIGNAR_VALOR");
-            NonTerminal CONSTANTE = new NonTerminal("CONSTANTE");
-            NonTerminal OTRA_CONSTANTE = new NonTerminal("OTRA_CONSTANTE");
-            NonTerminal TIPO = new NonTerminal("TIPO");
-            NonTerminal TRETORNO = new NonTerminal("TRETORNO");
-            NonTerminal OTRA_DECL_VARIABLE = new NonTerminal("OTRA_DECL_VARIABLE");
-            NonTerminal FUNCION_O_PROCEDIMIENTO = new NonTerminal("FUNCION_O_PROCEDIMIENTO");
-            NonTerminal FUNCION = new NonTerminal("FUNCION");
-            NonTerminal OTRA_FUNCION = new NonTerminal("OTRA_FUNCION");
+            NonTerminal INI = new NonTerminal(noterminales.INI);
+            NonTerminal COSAS = new NonTerminal(noterminales.COSAS);
+            NonTerminal USES = new NonTerminal(noterminales.USES);
+            NonTerminal OTRO_USES = new NonTerminal(noterminales.OTRO_USES);
+            NonTerminal CUERPO_PROGRAMA = new NonTerminal(noterminales.CUERPO_PROGRAMA);
+            NonTerminal INSTRUCCIONES = new NonTerminal(noterminales.INSTRUCCIONES);            
+            NonTerminal INSTRUCCION = new NonTerminal(noterminales.INSTRUCCION);
+            NonTerminal VARIABLE = new NonTerminal(noterminales.VARIABLE);
+            NonTerminal ASIGNAR_VALOR = new NonTerminal(noterminales.ASIGNAR_VALOR);
+            NonTerminal CONSTANTE = new NonTerminal(noterminales.CONSTANTE);
+            NonTerminal OTRA_CONSTANTE = new NonTerminal(noterminales.OTRA_CONSTANTE);
+            NonTerminal TIPO = new NonTerminal(noterminales.TIPO);
+            NonTerminal TRETORNO = new NonTerminal(noterminales.TRETORNO);
+            NonTerminal OTRA_DECL_VARIABLE = new NonTerminal(noterminales.OTRA_DECL_VARIABLE);
+            NonTerminal FUNCION_O_PROCEDIMIENTO = new NonTerminal(noterminales.FUNCION_O_PROCEDIMIENTO);
+            NonTerminal FUNCION = new NonTerminal(noterminales.FUNCION);
+            NonTerminal OTRA_FUNCION = new NonTerminal(noterminales.OTRA_FUNCION);
 
-            NonTerminal SENTENCIAS = new NonTerminal("SENTENCIAS");
-            NonTerminal SENTENCIA = new NonTerminal("SENTENCIA");
+            NonTerminal SENTENCIAS = new NonTerminal(noterminales.SENTENCIAS);
+            NonTerminal SENTENCIA = new NonTerminal(noterminales.SENTENCIA);
 
-            NonTerminal EXPRESION = new NonTerminal("EXPRESION");
+            NonTerminal EXPRESION = new NonTerminal(noterminales.EXPRESION);
 
             #endregion
 
